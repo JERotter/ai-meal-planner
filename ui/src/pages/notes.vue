120b9d04fcb2f13c7de5610fbea6b9f520b3e248
jenell <Jenell.E.Rotter@phila.gov>
2025-02-26 09:31:09 -0500

    updated some ui packages & added minor eslint corrections

--- ui/src/pages/notes.vue
+++ ui/src/pages/notes.vue
@@ -1,22 +1,24 @@
 <template>
-  <v-container>
-    <h2>Color Palette 1</h2>
-    <div v-for="color in colors" :key="color.name">
-      <ColorSwatch :colorName="color.name" :colorValue="color.value" />
-    </div>
-  </v-container>
-  <v-container>
-    <h2>Color Palette 2</h2>
-    <div v-for="color in colors2" :key="color.name">
-      <ColorSwatch :colorName="color.name" :colorValue="color.value" />
-    </div>
-  </v-container>
-  <v-container>
-    <h2>Color Palette 3/Cozies</h2>
-    <div v-for="color in colors3" :key="color.name">
-      <ColorSwatch :colorName="color.name" :colorValue="color.value" />
-    </div>
-  </v-container>
+  <div>
+    <v-container>
+      <h2>Color Palette 1</h2>
+      <div v-for="color in colors" :key="color.name">
+        <ColorSwatch :colorName="color.name" :colorValue="color.value" />
+      </div>
+    </v-container>
+    <v-container>
+      <h2>Color Palette 2</h2>
+      <div v-for="color in colors2" :key="color.name">
+        <ColorSwatch :colorName="color.name" :colorValue="color.value" />
+      </div>
+    </v-container>
+    <v-container>
+      <h2>Color Palette 3/Cozies</h2>
+      <div v-for="color in colors3" :key="color.name">
+        <ColorSwatch :colorName="color.name" :colorValue="color.value" />
+      </div>
+    </v-container>
+  </div>
 </template>
 
 <script setup>
