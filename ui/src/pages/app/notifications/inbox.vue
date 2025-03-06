4cba0f3b3145ed9860037ad8a238106820be68f1
jenell <Jenell.E.Rotter@phila.gov>
2025-03-06 11:51:10 -0500

    replaced inbox template

---
+++ ui/src/pages/app/notifications/inbox.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <!-- <Notifications /> -->
+    <Notifications />
+    <AppFooter />
+  </div>
+</template>
+
+<script setup>
+import Notifications from '@/components/Notifications.vue';
+
+</script>
