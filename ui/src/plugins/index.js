885eb434cba2e2adff20c8df175fe04ffc0c3430
jenell <Jenell.E.Rotter@phila.gov>
2025-06-27 08:33:57 -0400

    imported vue-good-table

--- ui/src/plugins/index.js
+++ ui/src/plugins/index.js
@@ -6,12 +6,14 @@
 //Plugins
 import vuetify from './vuetify'
 import router from '@/router'
+import VueGoodTablePlugin from 'vue-good-table'
+import 'vue-good-table/dist/vue-good-table.css'
 import { createPinia } from 'pinia'
-
 
 export function registerPlugins (app) {
   app
     .use(vuetify)
     .use(router)
+    .use(VueGoodTablePlugin)
     .use(createPinia())
 }
